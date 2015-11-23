106612cd84514b6a3e4ef6dc5e06fa0394330c12
mike <mike@vatbox.com>
2015-11-23 16:14:36 +0200

    - adding transactions to speed up benchmark setup

--- test/performance/search_test.rb
+++ test/performance/search_test.rb
@@ -3,17 +3,22 @@ require 'rails/performance_test_help'
 
 class SearchTest < ActionDispatch::PerformanceTest
   setup do
-    number_of_skills = 100
-    number_of_workers = 1000
+    number_of_skills = 30
+    number_of_workers = 300
     number_of_skills_to_match = 3
 
     DatabaseCleaner.clean
-    @skills = number_of_skills.times.collect {|i| Skill.create(keyword: "#{Faker::Company.profession} #{i}") }
-    @workers = number_of_workers.times.collect {|i| Worker.create(email: "#{i}#{Faker::Internet.email}", name: "#{Faker::Name.name}")}
-    @workers.each do |w|
+
+    Neo4j::Transaction.run do
+      @skills = number_of_skills.times.collect {|i| Skill.create(keyword: "#{Faker::Company.profession} #{i}") }
+      @workers = number_of_workers.times.collect {|i| Worker.create(email: "#{i}#{Faker::Internet.email}", name: "#{Faker::Name.name}")}
+    end
+    Neo4j::Transaction.run do
+      @workers.each do |w|
       # assign random number of skills per worker
       skills_count = Random.rand(10)
       w.skills = @skills.sample(skills_count)
+      end
     end
     @skills_to_find = @skills.sample(number_of_skills_to_match).map(&:keyword)
   end
